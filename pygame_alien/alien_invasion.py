010fa653350ab3e854be7e51e45df4ae28418f4c
penny88111 <53985914+penny88111@users.noreply.github.com>
2020-04-24 14:38:48 -0400

    Update alien_invasion.py

--- pygame_alien/alien_invasion.py
+++ pygame_alien/alien_invasion.py
@@ -9,16 +9,16 @@ def run_game():
     pygame.init()
     pygame.display.set_caption("Alien Invasion")
     screen = pygame.display.set_mode((1200, 800))
-#
-#     while True:
-#
-#         for event in pygame.event.get():
-#             if event.type == pygame.QUIT:
-#                 sys.exit()
-#
-#         pygame.display.flip()
-#
-# run_game()
+
+    while True:
+
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+
+        pygame.display.flip()
+
+run_game()
 #
 # #
 
